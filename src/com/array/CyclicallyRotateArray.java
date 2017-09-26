27eee21cb96bca23812424df0926549ce93a2c22
Manoj Kumar <manoj.baghela@affle.com>
2017-09-26 21:49:37 +0530

    Cyclically Rorate Array

---
+++ src/com/array/CyclicallyRotateArray.java
@@ -0,0 +1,36 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class CyclicallyRotateArray {
+
+	
+	
+	
+	/*
+	
+	Program to cyclically rotate an array by one
+	
+	Input:  arr[] = {1, 2, 3, 4, 5}
+    Output: arr[] = {5, 1, 2, 3, 4}
+	*/
+	
+	static int[] array = {1, 2, 3, 4, 5, 6};
+	
+	public static void main(String[] arg){
+		System.out.println(Arrays.toString(array));
+		rotated();
+		System.out.println(Arrays.toString(array));
+	}
+	
+	public static void rotated(){
+		int x =  array[array.length-1];
+		
+		for (int j = array.length-1; j > 0; j--) {
+			array[j] = array[j-1];
+			
+		}
+		array[0] = x;
+	}
+
+}
